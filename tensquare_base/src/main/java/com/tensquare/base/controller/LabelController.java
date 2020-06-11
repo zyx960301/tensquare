fd4dba5cc4fc6e3bb35ae9672c72ab4b69ee65fe
yuxin <574731646@qq.com>
2020-06-11 14:16:34 +0800

    按条件查询

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/label")
@@ -17,55 +18,71 @@ public class LabelController {
 
     /**
      * 查询全部列表
+     *
      * @return
      */
     @RequestMapping(method = RequestMethod.GET)
-    public Result findAll(){
-        return new Result(true, StatusCode.OK,"查询成功",labelService.findAll());
+    public Result findAll() {
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
     }
 
     /**
      * 根据id查询标签
+     *
      * @param id
      * @return
      */
-    @RequestMapping(value ="/{id}", method = RequestMethod.GET)
-    public Result findById(@PathVariable String id){
-        return new Result(true, StatusCode.OK,"查询成功",labelService.findById(id));
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Result findById(@PathVariable String id) {
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findById(id));
     }
 
     /**
      * 添加标签
+     *
      * @param label
      * @return
      */
-    @RequestMapping( method = RequestMethod.POST)
-    public Result add(@RequestBody Label label){
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(@RequestBody Label label) {
         labelService.add(label);
-        return new Result(true,StatusCode.OK,"添加成功");
+        return new Result(true, StatusCode.OK, "添加成功");
     }
 
     /**
      * 修改标签
+     *
      * @param label
      * @param id
      * @return
      */
-    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
-    public Result update(@RequestBody Label label, @PathVariable String  id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Result update(@RequestBody Label label, @PathVariable String id) {
         label.setId(id);
         labelService.update(label);
-        return new Result(true,StatusCode.OK,"修改成功");
+        return new Result(true, StatusCode.OK, "修改成功");
     }
 
     /**
      * 删除标签
+     *
      * @param id
      * @return
      */
-    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
-     public Result deleteById(@PathVariable String id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String id) {
         labelService.deleteById(id);
-        return new Result(true,StatusCode.OK,"删除成功");
-     }
+        return new Result(true, StatusCode.OK, "删除成功");
     }
+
+    /**
+     * 根据条件查询
+     *
+     * @param searchMap
+     * @return
+     */
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public Result findSearch(@RequestBody Map searchMap) {
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findSearch(searchMap));
+    }
+}
--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -4,9 +4,16 @@ import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import entity.IdWorker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class LabelService {
@@ -61,5 +68,32 @@ public class LabelService {
      */
     public void deleteById(String id) {
         labelDao.deleteById(id);
+    }
+
+    /**
+     * 查询条件
+     * @param searchMap
+     * @return
+     */
+    public List<Label> findSearch(Map searchMap) {
+        return labelDao.findAll(new Specification<Label>() {
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
+                List<Predicate> predicateList = new ArrayList<>();
+                //标签名称查询
+                if(searchMap.get("labelname")!=null && !"".equals(searchMap.get("labelname"))){
+                    predicateList.add(cb.like(root.get("labelname").as(String.class),"%"+(String)searchMap.get("labelname")+"%"));
+                };
+                //状态查询
+                if(searchMap.get("state")!=null && !"".equals(searchMap.get("state"))){
+                    predicateList.add(cb.equal(root.get("state").as(String.class),(String)searchMap.get("state")));
+                };
+                //状态查询
+                if(searchMap.get("recommend")!=null && !"".equals(searchMap.get("recommend"))){
+                    predicateList.add(cb.equal(root.get("recommend").as(String.class),(String)searchMap.get("recommend")));
+                };
+                return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
+            }
+        });
     }
 }
